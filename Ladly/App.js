1413cc193749b97d6b295e43c9a06a52097b41ec
Karol-Nutifafa <knyagbodza@st.ug.edu.gh.>
2024-05-25 18:19:52 +0000

    Final work

---
+++ Ladly/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.sentence}>My name is  
+      <Text style={styles.name}>Martin</Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0000FF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sentence: {
+    fontSize: 24,
+  },
+  name: {
+    fontWeight: 'bold',
+  }
+});
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# -rn-assignment2-ID-11060940
+# -rn-assignment2-ID-
+
+![photo_2024-05-25_16-33-29](https://github.com/Karol-Nutifafa/-rn-assignment2-ID-11060940/assets/151832041/03036203-387c-4815-8b07-5be03f82851e)
